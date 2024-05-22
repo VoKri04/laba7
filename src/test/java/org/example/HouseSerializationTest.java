520afcfcbebdf709f607b2d6d75c81351f92cca9
VoKri04 <vpkrivih@gmail.com>
2024-05-22 14:14:05 +0600

    tests changed for 6 and 8 tasks

--- src/test/java/org/example/HouseSerializationTest.java
+++ src/test/java/org/example/HouseSerializationTest.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import com.google.gson.Gson;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
